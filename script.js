7e1f3bb9a24c2edeec3d68586d15c013bfd85eb8
Harsh9699 <106963861+Harsh9699@users.noreply.github.com>
2026-03-06 16:37:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,29 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 // SIGNUP
 const signupForm = document.getElementById("signup-form");
 
-if(signupForm){
-signupForm.addEventListener("submit", (e)=>{
+if (signupForm) {
+
+signupForm.addEventListener("submit", function(e){
 e.preventDefault();
 
 const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
 
-createUserWithEmailAndPassword(auth,email,password)
-.then((userCredential)=>{
-alert("Account Created Successfully 🙏");
+createUserWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+
+alert("Account created successfully");
 window.location.href="dashboard.html";
+
 })
-.catch((error)=>{
+.catch((error) => {
+
 alert(error.message);
+
 });
 
 });
+
 }
 
 
@@ -30,21 +36,27 @@ alert(error.message);
 // LOGIN
 const loginForm = document.getElementById("login-form");
 
-if(loginForm){
-loginForm.addEventListener("submit",(e)=>{
+if (loginForm) {
+
+loginForm.addEventListener("submit", function(e){
 e.preventDefault();
 
 const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
 
-signInWithEmailAndPassword(auth,email,password)
-.then((userCredential)=>{
-alert("Login Successful 🙏");
+signInWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+
+alert("Login successful");
 window.location.href="dashboard.html";
+
 })
-.catch((error)=>{
+.catch((error) => {
+
 alert(error.message);
+
 });
 
 });
+
 }
