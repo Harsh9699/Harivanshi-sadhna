6fa72596e7ad650d2dd7cd7e8ad7c358a0892cd5
Harsh9699 <106963861+Harsh9699@users.noreply.github.com>
2026-03-06 16:32:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,6 @@
-// Firebase SDK imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAb0xwt31jRcL4J5e3SyJOQznMqBdrab3U",
   authDomain: "harivanshi-sadhna.firebaseapp.com",
@@ -16,13 +10,6 @@ const firebaseConfig = {
   appId: "1:441888438716:web:6167ecf717821e4767d3b9"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// Export services
-export { auth, db };
+export const auth = getAuth(app);
